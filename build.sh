2936cdf4c9eca7347036097d2723fb6c89881a1d
iduane <iduane@126.com>
2018-01-02 12:19:33 +0800

    add bulid script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm use v6.1.0
+
+npm run build-lib
